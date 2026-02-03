ffb8d2c0f001ac169ec5d610b6e8125f972eb8ba
josh <jboneplus7t@gmail.com>
2026-02-03 08:10:00 +0530

    final with log service

--- server/logger/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ server/logger/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e26e523883fb4c5c36892e7305d6bf8ff08e0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
